59e74a8276b209f23ca738066521ede9229ea6fb
Ummaiirrr <umairqaiser124@gmail.com>
2026-07-19 21:47:08 +0500

    chore: untrack scratch/ and handoff doc, now gitignored

--- scratch/check_policies.py
+++
@@ -1,7 +0,0 @@
-import psycopg2
-conn = psycopg2.connect('postgresql://postgres.ksdcjwpbusadklpdwfsz:testnigheban@aws-1-ap-south-1.pooler.supabase.com:6543/postgres')
-cur = conn.cursor()
-cur.execute("SELECT tablename, policyname, roles, cmd, qual FROM pg_policies WHERE tablename IN ('district', 'glacial_lake', 'drought_index')")
-for row in cur.fetchall():
-    print(row)
-conn.close()
